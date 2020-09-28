60959c0da619d602bccd3469f978bfddeddb7d0f
Dipto <diptobiswas1998@gmail.com>
2020-09-28 22:56:35 +0530

    [Dipto] refactor: Checking if two lines have equal length or not

--- Line_Comparison_Problem/Line_Comparison_Problem/Program.cs
+++ Line_Comparison_Problem/Line_Comparison_Problem/Program.cs
@@ -5,30 +5,58 @@ namespace Line_Problem
     class Program
     {
 
+        static float CalulateLength(int x1, int x2, int y1, int y2)
+        {
+            float length = (float)Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+            return length;
+        }
+
         static void Main(string[] args)
         {
             //Welcome msg
             Console.WriteLine("Welcome to Line Comparison Problem!");
             
             //variables
-            int x1 = 0, x2 = 0, y1 = 0, y2 = 0;
+
+            float[] length = new float[2];
 
 
-            Console.WriteLine("Enter the coordinates of 1st point");
+            //Taking coordinate inputs from the user for 2 lines
+            for (int i = 0; i <= 1; i++)
+            {
+                int x1 = 0, x2 = 0, y1 = 0, y2 = 0;
 
-            x1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the coordinates of Line " + (i + 1));
 
-            y1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the coordinates of X");
 
-            Console.WriteLine("Enter the coordinates of 2nd point");
+                x1 = Convert.ToInt32(Console.ReadLine());
 
-            x2 = Convert.ToInt32(Console.ReadLine());
+                x2 = Convert.ToInt32(Console.ReadLine());
 
-            y2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the coordinates of Y");
 
-            float length = (float)Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+                y1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Legth of line = " + length);
+                y2 = Convert.ToInt32(Console.ReadLine());
+
+                length[i] = CalulateLength(x1, x2, y1, y2);
+
+            }
+
+
+
+            if (length[0].Equals(length[1]))
+            {
+                Console.WriteLine("Lines have equal length.");
+            }
+            else
+            {
+                Console.WriteLine("Lines are not equal in length!");
+            }
+
+
+
         }
     }
 }
