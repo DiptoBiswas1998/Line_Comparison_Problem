dbea6cf580b00a7ee97f1f32bd2abc3798bf5115
Dipto <diptobiswas1998@gmail.com>
2020-09-28 22:39:34 +0530

    [Dipto] refactor: Calculating Length

--- Line_Comparison_Problem/Line_Comparison_Problem/Program.cs
+++ Line_Comparison_Problem/Line_Comparison_Problem/Program.cs
@@ -1,13 +1,34 @@
 ﻿using System;
 
-namespace Line_Comparison_Problem
+namespace Line_Problem
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             //Welcome msg
             Console.WriteLine("Welcome to Line Comparison Problem!");
+            
+            //variables
+            int x1 = 0, x2 = 0, y1 = 0, y2 = 0;
+
+
+            Console.WriteLine("Enter the coordinates of 1st point");
+
+            x1 = Convert.ToInt32(Console.ReadLine());
+
+            y1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the coordinates of 2nd point");
+
+            x2 = Convert.ToInt32(Console.ReadLine());
+
+            y2 = Convert.ToInt32(Console.ReadLine());
+
+            float length = (float)Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+
+            Console.WriteLine("Legth of line = " + length);
         }
     }
 }
